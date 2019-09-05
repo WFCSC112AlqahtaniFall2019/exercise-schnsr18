1b783786a8c06b0930f3dd04db9c31017c591347
sophieschneider <schnsr18@wfu.edu>
2019-09-05 15:47:46 -0400

    classwork

---
+++ cmake-build-debug/CMakeFiles/VectorReverse.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VectorReverse.dir/main.cpp.o"
+  "VectorReverse.pdb"
+  "VectorReverse"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VectorReverse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVctr[8];               // User values
-    int i;                        // Loop index
+    vector <int>revVctr(8);               // User values
+    int i;
+    int temp;
+
+    // Loop index
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cin >> revVctr[i];
+        cin >> revVctr.at(i);
     }
 
     // Reverse
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+    for (i = 0; i < (revVctr.size() / 2); ++i) {
+        temp = revVctr.at(i);  // These 3 statements swap
+        revVctr.at(i) = revVctr.at(revVctr.size() - 1 - i);
+        revVctr.at(revVctr.size() - 1 - i) = temp;
     }
 
     // Print values
     cout << endl << "New values: ";
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cout << " " << revVctr[i];
+        cout << " " << revVctr.at(i);
     }
     cout << endl;
 
